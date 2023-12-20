ebc0faafaaa4eab9a90177e64bbd3f1a496bc70e
girimana <girimajudica@hotmail.com>
2023-12-20 16:28:53 +0200

    modif

--- core/scheduler.py
+++ core/scheduler.py
@@ -7,8 +7,8 @@ from pytz import timezone
 def start_send_invoice_offline_job():
     scheduler = BackgroundScheduler({'apscheduler.timezone': timezone('Africa/Bujumbura')}  )
 
-    scheduler.add_job(send_invoice_offline, 'interval', seconds=10)
-    scheduler.add_job(send_stock_offline, 'interval', seconds=10)
+    scheduler.add_job(send_invoice_offline, 'interval', minutes=1)
+    scheduler.add_job(send_stock_offline, 'interval', minutes=1)
      
     scheduler.start()
     
